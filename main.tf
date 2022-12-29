2cd54f76fadf16bf03cd61edb127699d4939252e
sakthibazz1 <sakthiglmech123@gmail.com>
2022-12-29 14:51:45 +0530

    specified the about the code

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
+//we are using AWS provider 
 provider "aws" {
-    region = "us-east-1"
-
-     
+    region = "us-east-1"     
 }
 
+//creating one vpc and assinging cidr block
 resource "aws_vpc" "tf-vpc" {
     cidr_block = "10.0.0.0/16"
     tags = {
@@ -11,6 +11,7 @@ resource "aws_vpc" "tf-vpc" {
     }  
 }
 
+//creating subnet and attached to vpc created above
 resource "aws_subnet" "tf-subnet" {
     vpc_id = aws_vpc.tf-vpc.id
     availability_zone = "us-east-1a"
@@ -20,24 +21,26 @@ resource "aws_subnet" "tf-subnet" {
     }
 }
 
+//creating route table and adding internet gateway to route table
 resource "aws_route_table" "tf-rt" {
     vpc_id = aws_vpc.tf-vpc.id
     route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.tf-ig.id
-  }
+    }
     tags ={
       "Name" = "tf-rt"
     }  
 }
 
+//associating subnet with route table
 resource "aws_route_table_association" "tf-rt-association-subnet" {
     route_table_id = aws_route_table.tf-rt.id
     subnet_id = aws_subnet.tf-subnet.id  
 }
 
 
-
+//creating internet gateway for vpc 
 resource "aws_internet_gateway" "tf-ig" {
     vpc_id = aws_vpc.tf-vpc.id
     tags = {
@@ -45,6 +48,7 @@ resource "aws_internet_gateway" "tf-ig" {
     }  
 }
 
+//specifying the inbound rules of security group
 resource "aws_security_group_rule" "tf-sg-inrule" {
   type              = "ingress"
   from_port         = 0
@@ -54,6 +58,7 @@ resource "aws_security_group_rule" "tf-sg-inrule" {
   security_group_id = aws_security_group.tf-sg.id
 }
 
+//specifying the outbound rule of security group
 resource "aws_security_group_rule" "tf-sg-outrule" {
   type              = "egress"
   from_port         = 0
@@ -63,7 +68,7 @@ resource "aws_security_group_rule" "tf-sg-outrule" {
   security_group_id = aws_security_group.tf-sg.id
 }
 
-
+//creating security group for vpc
 resource "aws_security_group" "tf-sg" {
     description = "Allow ssh,http,https traffic"
     vpc_id = aws_vpc.tf-vpc.id
@@ -72,6 +77,7 @@ resource "aws_security_group" "tf-sg" {
     }
 }
 
+//creating ec2-instance
 resource "aws_instance" "tf-ec2" {
   ami           = var.ami
   instance_type = var.instance_type
@@ -87,10 +93,13 @@ resource "aws_instance" "tf-ec2" {
   }
   user_data = "${file("userdata.txt")}"  
 }
+
+//attaching the security group  with instance
 resource "aws_network_interface_sg_attachment" "tf-sg-add" {
   network_interface_id = aws_instance.tf-ec2.primary_network_interface_id
   security_group_id = aws_security_group.tf-sg.id  
 }
+//the below commented lines are for allocate elastic-ip address for instance,if u need uncommented it.
 # # resource "aws_eip" "tf-eip" {
 # #   vpc = true
   
@@ -100,14 +109,20 @@ resource "aws_network_interface_sg_attachment" "tf-sg-add" {
 #   allocation_id = aws_eip.tf-eip.id
   
 # }
+
+//creating keypair
 resource "aws_key_pair" "tf-key-pair" {
   key_name = "tf-key-pair"
   public_key = tls_private_key.rsa.public_key_openssh
   }
+
+//specifying the format of the keypair
 resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits  = 4096
   }
+
+//saving the keypair to local
 resource "local_file" "tf-key" {
   content  = tls_private_key.rsa.private_key_pem
   filename = "tf-key-pair"
